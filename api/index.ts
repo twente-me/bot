da7eb097cb0994087a86d778b8ea30915a0dae92
Anand Chowdhary <mail@anandchowdhary.com>
2020-02-02 13:29:05 +0100

    :sparkles: Add API key authentication

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,12 @@ import { NowRequest, NowResponse } from "@now/node";
 import { update } from "../src/update";
 
 export default async (req: NowRequest, res: NowResponse) => {
+  if (req.query.key !== process.env.TWENTEBOT_KEY)
+    return res.status(401).json({
+      error: "invalid_api_key",
+      message: "You've supplied an invalid API key to use Twente Bot"
+    });
+
   try {
     const result = await update();
     res.json({ success: true, result });
