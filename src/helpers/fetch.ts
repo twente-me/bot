38f8cf532ac2fe6bc7bcad9b25703a7923a1e51d
Anand Chowdhary <anandchowdhary@gmail.com>
2020-02-02 15:53:17 +0100

    :bug: Remove .cache dir
    On Now, you get this error: `{"error":{"errno":-30,"code":"EROFS","syscall":"mkdir","path":"/var/task/src/helpers/.cache"}}` when trying to create a folder in __dirname

--- src/helpers/fetch.ts
+++ src/helpers/fetch.ts
@@ -5,15 +5,8 @@ import { urlToCache } from "./utils";
 import { logEvent } from "./logger";
 
 export const cachedJson = async (url: string) => {
-  const cachePath = join(__dirname, ".cache", urlToCache(url));
-  await ensureDir(join(cachePath, ".."));
-  try {
-    const cachedResult = await readJson(cachePath);
-    if (cachedResult) return cachedResult;
-  } catch {}
   logEvent(`Fetching ${url}`);
   const response = await axios(url);
   const body = response.data;
-  await writeJson(cachePath, body);
   return body;
 };
